d4536ed98b6a2639ec2c775816020e7b6c5754ba
yaroslavstepanov <yaroslavstepanov2018@gmail.com>
2021-07-08 00:25:10 +0300

    Комментарии

--- todolist/app.js
+++ todolist/app.js
@@ -2,40 +2,46 @@ let buttonEnter = document.getElementById('enter');
 let userInput = document.getElementById('userInput');
 let ul = document.querySelector('ul');
 
+//Функция проверки поля ввода на пустоту.
 function inputLength() {
     return userInput.value.length > 0;
 }
 
+//Функция для создания и удаления заметок.
 function createTodo() {
-    let li = document.createElement("li");
+    let li = document.createElement("li");//Создание заметки.
     li.appendChild(document.createTextNode(userInput.value));
     ul.appendChild(li);
     userInput.value = '';
 
-    let deleteButton = document.createElement('button');
+    let deleteButton = document.createElement('button');//Удаление заметки.
     deleteButton.appendChild(document.createTextNode('X'));
     li.appendChild(deleteButton);
     deleteButton.addEventListener('click', deleteTodoItem);
 
-    let colorPress = document.createElement('button')
+    let colorPress = document.createElement('button')//Смена цвета заметки.
     li.appendChild(colorPress);
     colorPress.addEventListener('click', ColorTodoItem);
 
+    //Удаление заметки.
     function deleteTodoItem() {
         li.classList.add('delete');
     }
+
+    //Смена цвета заметки.
     function ColorTodoItem(){
         li.classList.toggle('done');
     }
-
 }
 
+//Добавление заметки по нажатию на клавишу 'Enter'.
 function changeListAfterKeyPress(event) {
     if (inputLength() && event.which === 13) {
         createTodo();
     }
 }
 
+//Добавление заметки по нажатию на кнопку на форме.
 function changeListAfterButtonPress() {
     if (inputLength()) {
         createTodo();
