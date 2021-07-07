12b026866f6de3b9944a588d94ecaf3d3d82dcdc
yaroslavstepanov <yaroslavstepanov2018@gmail.com>
2021-07-07 20:29:08 +0300

    Классная+ДЗ

---
+++ todolist/app.js
@@ -0,0 +1,44 @@
+let buttonEnter = document.getElementById('enter');
+let userInput = document.getElementById('userInput');
+let ul = document.querySelector('ul');
+
+function inputLength() {
+    return userInput.value.length > 0;
+}
+
+function createTodo() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(userInput.value));
+    ul.appendChild(li);
+    userInput.value = '';
+
+    let deleteButton = document.createElement('button');
+    deleteButton.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener('click', deleteTodoItem);
+
+    li.addEventListener('click', function() {
+        li.classList.toggle('done');
+    })
+
+    function deleteTodoItem() {
+        li.classList.add('delete');
+    }
+
+}
+
+function changeListAfterKeypress(event) {
+    if (inputLength() && event.which === 13) {
+        createTodo();
+    }
+}
+
+function changeListAfterButtonPress() {
+    if (inputLength()) {
+        createTodo();
+    }
+}
+
+buttonEnter.addEventListener('click', changeListAfterButtonPress);
+
+userInput.addEventListener('keypress', changeListAfterKeypress);
