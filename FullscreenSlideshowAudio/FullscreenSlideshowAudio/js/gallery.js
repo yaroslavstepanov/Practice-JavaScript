1224e8ad49bcf52593dc6a992c47504e5e5a891d
yaroslavstepanov <yaroslavstepanov2018@gmail.com>
2021-07-09 19:42:03 +0300

    FullscreenSlideshowAudio

---
+++ FullscreenSlideshowAudio/FullscreenSlideshowAudio/js/gallery.js
@@ -0,0 +1,24 @@
+$( function() {
+    var pictures = [],
+        $pointer = $( '#pointer' ),
+        $thumbnails = $( '#thumbnails' ),
+        $title = $( '#title' ),
+        $pause = $( '#pause' ),
+        $flash = $( '#flash' ),
+        $volume = $( '#volume' );
+
+    // Buzz audio library
+
+
+    // jScrollPane
+
+
+    // Vegas Background
+    
+
+    // Volume button
+
+
+    // Photograph
+
+});
